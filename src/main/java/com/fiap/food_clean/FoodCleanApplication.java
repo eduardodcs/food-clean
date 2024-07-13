1d712b3d1b5b7a0c32cdd8123c712c05d1ff864d
Eduardo Silva <eddcesilva@gmail.com>
2024-07-13 14:39:08 -0300

    Projeto clean architecture

---
+++ src/main/java/com/fiap/food_clean/FoodCleanApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.food_clean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/food_clean/FoodCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.food_clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
